b15a83690c94822bb54ae46766e25367830b7631
developer-at-597849092155 <cloudreinforce@gmail.com>
2023-07-28 19:54:40 +0800

    updated the shell script

--- convert_case.sh
+++ convert_case.sh
@@ -5,6 +5,7 @@ BRANCH=${2}
 string="Increment and Decrement counter"
 convert_to_upper=`echo "$string" | awk '{print toupper($0)}'`
 #echo $convert_to_upper
+git pull origin master
 sed -i 's/Increment and Decrement counter/INCREMENT AND DECREMENT COUNTER/g' ./web_content/index.html
 git checkout -b $BRANCH master
 git add .
