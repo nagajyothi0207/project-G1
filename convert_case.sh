158e7926da931d967b494e85d91197bcd3eda54d
developer-at-597849092155 <cloudreinforce@gmail.com>
2023-07-28 19:38:28 +0800

    adding updated to code main branch before changing the website content for PR to master branch

--- convert_case.sh
+++ convert_case.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+set -e
+set -o pipefail
+BRANCH=${2}
 string="Increment and Decrement counter"
 convert_to_upper=`echo "$string" | awk '{print toupper($0)}'`
 #echo $convert_to_upper
 sed -i 's/Increment and Decrement counter/INCREMENT AND DECREMENT COUNTER/g' ./web_content/index.html
-
+git checkout -b $BRANCH master
 git add .
 git commit -am "Website Content is converted to UPPER CASE"
-git push
+git push -o merge_request.create -o merge_request.target=master  origin $BRANCH
