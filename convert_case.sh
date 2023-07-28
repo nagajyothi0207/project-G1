b7347347c1a9500ed1e72192a34d0f89e7a76406
developer-at-597849092155 <cloudreinforce@gmail.com>
2023-07-28 19:18:01 +0800

    Website Content is converted to UPPER CASE

--- convert_case.sh
+++ convert_case.sh
@@ -2,3 +2,7 @@ string="Increment and Decrement counter"
 convert_to_upper=`echo "$string" | awk '{print toupper($0)}'`
 #echo $convert_to_upper
 sed -i 's/Increment and Decrement counter/INCREMENT AND DECREMENT COUNTER/g' ./web_content/index.html
+
+git add .
+git commit -am "Website Content is converted to UPPER CASE"
+git push
