f1451212b81d92cd856222ff9aa6c55e3004101d
developer-at-597849092155 <cloudreinforce@gmail.com>
2023-07-28 19:51:02 +0800

    changed to lower for testing

--- convert_case.sh
+++ convert_case.sh
@@ -9,4 +9,5 @@ sed -i 's/Increment and Decrement counter/INCREMENT AND DECREMENT COUNTER/g' ./w
 git checkout -b $BRANCH master
 git add .
 git commit -am "Website Content is converted to UPPER CASE"
+git push --set-upstream origin master
 git push -o merge_request.create -o merge_request.target=master  origin $BRANCH
