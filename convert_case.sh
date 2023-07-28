1380b8287d6012f8aa18563517b3256e60d27e5c
developer-at-597849092155 <cloudreinforce@gmail.com>
2023-07-28 23:11:23 +0800

    adding modified code to feature branch

--- convert_case.sh
+++ convert_case.sh
@@ -5,10 +5,14 @@ BRANCH=${2}
 string="Increment and Decrement counter"
 convert_to_upper=`echo "$string" | awk '{print toupper($0)}'`
 #echo $convert_to_upper
-git pull origin master
-sed -i 's/Increment and Decrement counter/INCREMENT AND DECREMENT COUNTER/g' ./web_content/index.html
-git checkout -b $BRANCH master
+git checkout main
+git pull origin main
+git checkout -b master main
 git add .
-git commit -am "Website Content is converted to UPPER CASE"
+git commit -am "Code rebase for pushing to Master Branch"
 git push --set-upstream origin master
-git push -o merge_request.create -o merge_request.target=master origin $BRANCH
+git checkout -b ${BRANCH} master
+git add .
+git commit -am "Website Content is converted to UPPER CASE and pushing to Master Branch"
+sed -i 's/Increment and Decrement counter/INCREMENT AND DECREMENT COUNTER/g' ./web_content/index.html
+git push -o merge_request.create -o merge_request.target=master origin ${BRANCH}
