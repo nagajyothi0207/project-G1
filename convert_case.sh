7133e4c5dbcc64891ea1f9d3fb6f6c56cfca79e8
developer-at-597849092155 <cloudreinforce@gmail.com>
2023-07-30 17:19:15 +0800

    Website Content is converted to UPPER CASE

--- convert_case.sh
+++ convert_case.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -o pipefail
-BRANCH=${2}
+BRANCH=${1}
 string="Increment and Decrement counter"
 convert_to_upper=`echo "$string" | awk '{print toupper($0)}'`
 #echo $convert_to_upper
@@ -11,6 +11,6 @@ sed -i 's/Increment and Decrement counter/INCREMENT AND DECREMENT COUNTER/g' ./w
 git checkout -b $BRANCH master
 git add .
 git commit -am "Website Content is converted to UPPER CASE"
-git push --set-upstream origin $BRANCH
 git push -o merge_request.create -o merge_request.target=master origin $BRANCH
-git checkout master
+#git checkout master
+#git push --set-upstream origin $BRANCH
